5b250c426f1afe2192616e3843396b610c6995f5
unknown <mwangiarthur777@gmail.com>
2023-05-22 13:36:53 +0300

    first commit

---
+++ account/api.py
@@ -0,0 +1,28 @@
+from django.http import JsonResponse
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+
+from .forms import SignUpForm
+
+@api_view(['POST'])
+@authentication_classes([])
+@permission_classes([])
+def signup(request):
+    data = request.data
+    message = 'success'
+
+    form = SignUpForm({
+        'name': data.get('name'),
+        'email': data.get('email'),
+        'password': data.get('password'),
+        'first_name': data.get('first_name'),
+        'last_name': data.get('last_name')
+    })
+
+    if form.is_valid():
+        form.save()
+
+        #Send Verification Email
+    else:
+        message = 'error'
+
+    return JsonResponse({'status': message})
